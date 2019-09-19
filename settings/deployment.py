ecb6b6b1f24198c91a1c287ffece8dd228c7f645
zaidHabiba <zaid-habiba@hotmail.com>
2019-09-19 16:37:39 +0300

    Fix

--- settings/deployment.py
+++ settings/deployment.py
@@ -93,7 +93,11 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+import django_heroku
+
+django_heroku.settings(locals())
--- settings/local.py
+++ settings/local.py
@@ -92,3 +92,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+STATIC_URL = '/static/'
