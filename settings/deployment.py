fa2a95e5ffd0ea4f04c9f1e33101dbd6cbacf319
zaidHabiba <zaid-habiba@hotmail.com>
2019-10-24 14:56:45 +0300

    update

--- settings/deployment.py
+++ settings/deployment.py
@@ -2,4 +2,15 @@ import django_heroku
 
 from .base import *
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 django_heroku.settings(locals())
--- settings/testing.py
+++
@@ -1,2 +0,0 @@
-from .base import *
-#Database url in CircleCi environment variables
